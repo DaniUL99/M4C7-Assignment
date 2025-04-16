19fa31fcfc9e0d74d78c49513111676abf809c50
DaniUL99 <83228977+DaniUL99@users.noreply.github.com>
2025-04-16 06:13:38 +0200

    Add files via upload

---
+++ M4C7_JavaScript.js
@@ -0,0 +1,10 @@
+function suma(num1,num2,num3,num4) {
+    console.log((num1 + num2) * (num3 + num4));
+}
+
+if (suma > 50) {
+    console.log("¡El número es mayor que 50!")
+} else {
+    console.log("¡El número es menor que 50!")
+}
+
